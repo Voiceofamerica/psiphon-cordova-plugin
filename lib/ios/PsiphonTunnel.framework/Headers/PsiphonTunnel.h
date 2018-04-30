dd7ed1fa5bb2049d0a4115f42010e34db6eeefa1
Kevin Groat <apps@kevingroat.io>
2018-04-30 16:40:04 -0400

    Updated psiphon framework for iOS

--- lib/ios/PsiphonTunnel.framework/Headers/PsiphonTunnel.h
+++ lib/ios/PsiphonTunnel.framework/Headers/PsiphonTunnel.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41368be02176816c64b022aaeb098dea6d0c0ca4a359c6817e49bc347fa19455
-size 16770
+oid sha256:c5f74c7ba4980c8e3e06601ce5509fe4380fa9b76fb22bce16c5b4deb8f763d3
+size 17259
